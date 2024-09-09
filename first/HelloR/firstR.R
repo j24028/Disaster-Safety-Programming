bacff63717e638c9d07e403aec2a609672f46f6f
j24028 <qufss04@gmail.com>
2024-09-09 16:17:34 +0900

    Add files via upload

---
+++ first/HelloR/firstR.R
@@ -0,0 +1,56 @@
+library(installr)
+check.for.updates.R() #최신버전인지 확인! 
+#install.R()
+#주석이당
+a = 1
+a <- 10
+a = 1
+b = 3
+a + b
+print(a+b) #print 잘 안씀. 주로 디버깅 할때만 씀 
+print("Hello")
+print("20")
+a <- 3
+b = 4
+c = 7
+d=a+b
+print(a)
+print(d)
+print("Hello")
+2^3
+2**3
+3%%2 #나머지
+5%/%2 #몫 
+3 >= 2
+5 != 5
+4 > 8
+43 >= 3434
+!TRUE
+a = TRUE
+isTRUE(a)
+c < b | a
+c == b & a
+12 < b | a == FALSE
+123 * -1 < c | a == isFALSE(a)
+1+2+3+4+5+6+7+8-123421%%233%/%2
+10/4
+(1-3)^2+(2-3)^2+(3-3)^2+(4-3)^2+(5-3)^2
+13%/%5
+e=234
+f <- -12
+g = FALSE
+(a == d)|(f <= 23)
+!(a == g)
+(5 <= 12) & (123 < d)
+(5 == 6) | (a < e) # 결과 : TRUE / why???????????????? a=TRUE, e =234
+integer_var <- 5
+numeric_var <- 3.14
+Complex_var<- 2+3i
+character_var <- "Hello World"
+logical_var <- TRUE
+another_logical_var <- FALSE
+NULL_var <- NULL
+NA_var <- NA
+negative_inf <- -Inf
+positive_inf <- Inf
+nan <- 0/0
